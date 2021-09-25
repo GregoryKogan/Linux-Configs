885722de735c424bc41d31fb10d0cf2101da3ce6
Gregory Kogan <60318411+GregoryKogan@users.noreply.github.com>
2021-09-25 12:42:17 +0000

    Sublime text added

--- install_software.sh
+++ install_software.sh
@@ -6,7 +6,6 @@ sudo pacman -S --needed --noconfirm alacritty
 sudo pacman -S --needed --noconfirm code
 sudo pacman -S --needed --noconfirm discord
 sudo pacman -S --needed --noconfirm firefox
-sudo pacman -S --needed --noconfirm sublime-text-4
 sudo pacman -S --needed --noconfirm telegram-desktop
 sudo pacman -S --needed --noconfirm xournalpp
 sudo pacman -S --needed --noconfirm cmatrix
@@ -114,3 +113,8 @@ rm -rf shell-color-scripts
 
 # Starship
 sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+
+# Sublime Text
+curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
+echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
+sudo pacman -Syu sublime-text
